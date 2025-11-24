2ef3e3f3397b6bec5293aece41c39281459fca33
AR2908 <162954754+AR2908@users.noreply.github.com>
2025-11-25 02:19:40 +0530

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -14,6 +14,17 @@ export default async function handler(req, res) {
       return;
     }
     // Yeh debug line paste karo:
-    res.status(200).json({ fields, files });
+   form.parse(req, (err, fields, files) => {
+  if (err) {
+    res.status(500).send("File upload error: " + err.message);
+    return;
+  }
+  // Extract file info from array:
+  const uploaded = files.file?.[0];
+  const filename = uploaded?.originalFilename || uploaded?.name || 'undefined';
+  res.status(200).send("File " + filename + " uploaded (temporary)!");
+});
+
   });
 }
+
