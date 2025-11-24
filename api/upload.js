976d92493da6ace6e61c48172119d03f28eb1ddb
AR2908 <162954754+AR2908@users.noreply.github.com>
2025-11-25 02:05:40 +0530

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,7 +1,5 @@
 import { IncomingForm } from "formidable";
 
-
-// Disable next.js body parser
 export const config = {
   api: {
     bodyParser: false,
@@ -13,15 +11,18 @@ export default async function handler(req, res) {
     res.status(405).send("Method not allowed");
     return;
   }
-const form = new IncomingForm({ multiples: false });
+  const form = new IncomingForm({ multiples: false });
 
-  
   form.parse(req, (err, fields, files) => {
     if (err) {
-      res.status(500).send("File upload error!");
+      res.status(500).send("File upload error: " + err.message);
       return;
     }
-    // Instead of saving to local, just check filename:
-    res.status(200).send("File " + files.file.originalFilename + " uploaded (temporary)!");
+
+    // Smart file key detection:
+    const uploaded = files.file || files['file'] || Object.values(files)[0];
+    const filename = uploaded?.originalFilename || uploaded?.name || 'undefined';
+
+    res.status(200).send("File " + filename + " uploaded (temporary)!");
   });
 }
